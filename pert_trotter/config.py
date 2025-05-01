14910b791c9321238e4128a9f408273f14e45f37
Shashank Mehendale <mehendalaesha@gmail.com>
2025-05-01 04:11:18 +0000

    Add configuration file to store global parameters

---
+++ pert_trotter/config.py
@@ -0,0 +1,6 @@
+# Configuration file to store some of the constants used by other modules. The values here needs to be updated appropriately before calling any functions that depend on these constants.
+
+num_elecs = None
+n_qubits = None
+savepath = 'data/'
+mol_name = None
