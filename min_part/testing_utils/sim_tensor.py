32d6f6398a12d04253c23179db70c52611db4051
lucyhao <hao.lucyy@gmail.com>
2025-06-21 09:31:19 -0400

    input generation

---
+++ min_part/testing_utils/sim_tensor.py
@@ -0,0 +1,263 @@
+import functools
+import itertools
+import operator
+
+import numpy as np
+import torch
+from fontTools.misc.py23 import isclose
+from hypothesis import strategies as st
+from openfermion import count_qubits
+
+from min_part.ham_utils import obtain_OF_hamiltonian
+from min_part.molecules import mol_h2
+
+
+@st.composite
+def symmetricND(draw, size: int) -> torch.Tensor:
+    data = torch.zeros(*[size] * 4)
+    for i in range(size):
+        for j in range(size):
+            for k in range(size):
+                for l in range(size):
+                    data[i, j, k, l] = draw(st.integers(0, 1))
+    return functools.reduce(
+        operator.add,
+        (
+            torch.permute(data, permutation)
+            for permutation in itertools.permutations(range(4))
+        ),
+    )
+
+
+@st.composite
+def artifical_h2_tbt(draw) -> np.ndarray:
+    lmao0 = draw(st.floats(-1, 1))
+    lmao1 = draw(st.floats(-1, 1))
+    lmao2 = draw(st.floats(-1, 1))
+    lmao3 = draw(st.floats(-1, 1))
+
+    return np.array(
+        [
+            [
+                [
+                    [lmao0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, lmao0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, lmao1 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, lmao1 + 0.0j],
+                ],
+                [
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                ],
+                [
+                    [0.0 + 0.0j, 0.0 + 0.0j, lmao2 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, lmao2 + 0.0j],
+                    [lmao2 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, lmao2 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                ],
+                [
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                ],
+            ],
+            [
+                [
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                ],
+                [
+                    [lmao0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, lmao0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, lmao1 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, lmao1 + 0.0j],
+                ],
+                [
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                ],
+                [
+                    [0.0 + 0.0j, 0.0 + 0.0j, lmao2 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, lmao2 + 0.0j],
+                    [lmao2 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, lmao2 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                ],
+            ],
+            [
+                [
+                    [0.0 + 0.0j, 0.0 + 0.0j, lmao2 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, lmao2 + 0.0j],
+                    [lmao2 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, lmao2 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                ],
+                [
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                ],
+                [
+                    [lmao1 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, lmao1 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, lmao3 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, lmao3 + 0.0j],
+                ],
+                [
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                ],
+            ],
+            [
+                [
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                ],
+                [
+                    [0.0 + 0.0j, 0.0 + 0.0j, lmao2 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, lmao2 + 0.0j],
+                    [lmao2 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, lmao2 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                ],
+                [
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                ],
+                [
+                    [lmao1 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, lmao1 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, lmao3 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, lmao3 + 0.0j],
+                ],
+            ],
+        ]
+    )
+
+
+def get_chem_tensors(H, N=None):
+    """
+    Obtain constant, one-body tensor and two body tensor in chemist ordering from a FermionOperator in physicist or chemist ordering. Assumes the existance of 8 fold symmetry of molecular Hamiltonians.
+
+    Args:
+        H (FermionOperator): FermionOperator
+        N (int, optional): Number of qubits. Defaults to None.
+
+    Returns:
+        float: Constant term in the Hamiltonian
+        ndarray: One body tensor
+        ndarray: Two body tensor in Chemist notation with all the appropriate (8) symmetries.
+    """
+
+    if N is None:
+        N = count_qubits(H)
+
+    for key, value in H.terms.items():
+        if len(key) == 4:
+            if key[1][1] == 1:  # Checking if the given term is physics ordered.
+                obt = np.zeros((N, N), dtype="complex128")
+                phy_tbt = np.zeros((N, N, N, N), dtype="complex128")
+                const = 0
+                obt_correction = np.zeros((N, N), dtype="complex128")
+
+                for key, value in H.terms.items():
+                    if len(key) == 4:
+                        phy_tbt[key[0][0], key[1][0], key[2][0], key[3][0]] += value
+                        if key[3][0] == key[1][0]:  # c0'c2'c1c2 = -c0'c2 + c0'c2c2'c1
+                            obt_correction[key[0][0], key[2][0]] -= value
+                    elif len(key) == 2:
+                        obt[key[0][0], key[1][0]] += value
+                    else:
+                        const += value
+
+                chem_tbt = np.transpose(phy_tbt, [0, 3, 1, 2])
+                chem_obt = obt + obt_correction
+                return const, chem_obt, chem_tbt
+
+            else:  # Assuming H is chemist ordered
+                chem_obt = np.zeros((N, N), dtype="complex128")
+                chem_tbt = np.zeros((N, N, N, N), dtype="complex128")
+                const = 0
+                for key, value in H.terms.items():
+                    if len(key) == 4:
+                        chem_tbt[key[0][0], key[1][0], key[2][0], key[3][0]] += (
+                            value / 2
+                        )
+                        chem_tbt[key[2][0], key[3][0], key[0][0], key[1][0]] += (
+                            value / 2
+                        )
+                    elif len(key) == 2:
+                        chem_obt[key[0][0], key[1][0]] += value
+                    else:
+                        const += value
+                return const, chem_obt, chem_tbt
+
+
+@st.composite
+def generate_symm_unitary_matrices(draw, n):
+    def is_special_unitary(m):
+        return (
+            isclose(np.linalg.det(m), 1)
+            and np.allclose(m @ m.T, np.identity(n))
+            and np.allclose(m, m.T)
+            and np.allclose(m, np.linalg.inv(m))
+        )
+
+    def is_symmetric(m):
+        return np.allclose(m, m.T)
+
+    def is_commute(q, l):
+        assert l.shape == (n, n)
+        return np.allclose(q @ l, l @ q)
+
+    i = 0
+    while True:
+        i += 1
+        A = np.array(
+            [
+                draw(st.lists(st.floats(-2, 2), min_size=n, max_size=n)),
+                draw(st.lists(st.floats(-2, 2), min_size=n, max_size=n)),
+                draw(st.lists(st.floats(-2, 2), min_size=n, max_size=n)),
+                draw(st.lists(st.floats(-2, 2), min_size=n, max_size=n)),
+            ]
+        )
+        A = (A + A.T) / 2
+        eigenvals, eigenvecs = np.linalg.eigh(A)
+        new_eigenvals = np.random.choice([-1, 1], size=n)
+        maybe_u = eigenvecs @ np.diag(new_eigenvals) @ eigenvecs.T
+        assert np.allclose(maybe_u, maybe_u.T)
+        diags = np.array(
+            draw(
+                st.lists(
+                    st.floats(-2, 2).filter(lambda x: not isclose(x, 0)),
+                    min_size=n,
+                    max_size=n,
+                )
+            )
+        )
+        maybe_symm = maybe_u @ np.diagflat(diags) @ maybe_u.T
+        if (
+            is_special_unitary(maybe_u)
+            and is_symmetric(maybe_symm)
+            and is_commute(maybe_u, np.diagflat(diags))
+        ):
+            break
+    return diags, maybe_u, maybe_symm
+
+
+def make_tensors(bond_length):
+    mol = mol_h2(bond_length)
+    H, num_elecs = obtain_OF_hamiltonian(mol)
+    n_qubits = count_qubits(H)
+    return get_chem_tensors(H=H, N=n_qubits)
--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -4,7 +4,7 @@ import numpy as np
 from hypothesis import given
 from opt_einsum import contract
 
-from min_part.tensor import generate_symm_unitary_matrices
+from min_part.testing_utils.sim_tensor import generate_symm_unitary_matrices
 
 
 class TensorTest(unittest.TestCase):
@@ -13,25 +13,35 @@ class TensorTest(unittest.TestCase):
         diags = u_symm[0]
         u = u_symm[1]
         symm = u_symm[2]
-        self.assertEqual(np.linalg.det(u), 1)
-        vals, vecs = np.linalg.eigh(symm)
-        np.testing.assert_array_equal(vecs @ np.diagflat(vals) @ vecs.T, vecs.T @ np.diagflat(vals) @ vecs)
-        np.testing.assert_array_equal(u @ np.diagflat(diags) @ u.T, u.T @ np.diagflat(diags) @ u)
-        self.assertEqual(vals.size, 4)
-        self.assertEqual(diags.size, 4)
-        np.testing.assert_array_equal(
-            contract("r,pr,qr->pq", vals, vecs, vecs),
-            contract("r,qr,pr->pq", vals, vecs, vecs),
+        np.testing.assert_array_almost_equal(
+            u @ np.diagflat(diags) @ u.T, u.T @ np.diagflat(diags) @ u
         )
-        np.testing.assert_array_equal(
-            contract("r,rp,rq->pq", vals, vecs, vecs),
-            contract("r,pr,qr->pq", vals, vecs, vecs),
-        )
-        np.testing.assert_array_equal(
+        np.testing.assert_array_almost_equal(
             contract("r,rp,rq->pq", diags, u, u),
             contract("r,pr,qr->pq", diags, u, u),
         )
-        np.testing.assert_array_equal(
-            contract("r,rp,rq->pq", diags, u, u),
-            contract("r,rp,rq->pq", vals, vecs, vecs),
+        # ===
+        vals, vecs = np.linalg.eigh(symm)
+        np.testing.assert_array_almost_equal(
+            contract("r,pr,qr->pq", vals, vecs, vecs),
+            contract("r,qr,pr->pq", vals, vecs, vecs),
+        )
+        try:
+            np.testing.assert_array_almost_equal(vecs, vecs.T)
+            np.testing.assert_array_almost_equal(
+                vecs @ np.diagflat(vals) @ vecs.T, vecs.T @ np.diagflat(vals) @ vecs
+            )
+            np.testing.assert_array_almost_equal(vecs, np.linalg.inv(vecs))
+            np.testing.assert_array_almost_equal(
+                contract("r,rp,rq->pq", vals, vecs, vecs),
+                contract("r,pr,qr->pq", vals, vecs, vecs),
+            )
+            self.assertAlmostEqual(np.linalg.det(vecs), 1)
+        except AssertionError:
+            pass
+        # ==
+
+        np.testing.assert_array_almost_equal(
+            contract("r,pr,qr->pq", diags, u, u),
+            contract("r,pr,qr->pq", vals, vecs, vecs),
         )
