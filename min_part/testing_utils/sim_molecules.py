9c7d8ebe617f47b464d5c45a5f7b655c8a88b752
lucyhao <hao.lucyy@gmail.com>
2025-06-21 09:32:00 -0400

    sep molecules from tensor gen

---
+++ min_part/testing_utils/sim_molecules.py
@@ -0,0 +1,28 @@
+from hypothesis import strategies as st
+
+from min_part.gfro_decomp import gfro_decomp
+from min_part.lr_decomp import lr_decomp
+from min_part.testing_utils.sim_tensor import make_tensors
+
+
+def specfic_gfro_decomp(bond_length):
+    H_const, H_obt, H_tbt = make_tensors(bond_length)
+    return H_obt, H_tbt, gfro_decomp(H_tbt), None
+
+
+@st.composite
+def H_2_LR(draw):
+    bond_length = draw(st.floats(0.2, 3))
+    H_const, H_obt, H_tbt = make_tensors(bond_length)
+    lr_h2_frags = lr_decomp(H_tbt)
+    print(f"bond length: {bond_length}")
+    return H_obt, H_tbt, lr_h2_frags, bond_length
+
+
+@st.composite
+def H_2_GFRO(draw):
+    bond_length = draw(st.floats(0.2, 3))
+    H_const, H_obt, H_tbt = make_tensors(bond_length)
+    gfro_h2_frags = gfro_decomp(H_tbt)
+    print(f"bond length: {bond_length}")
+    return H_obt, H_tbt, gfro_h2_frags, bond_length
